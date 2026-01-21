ee785caf9d87c208c029bf2c70043eb6f2c4d982
Yaping Xin <yaping.xin@foxmail.com>
2026-01-21 22:37:05 +0800

    用 Visual Studio 2026 编译和使用 QT 5.15.18 动态库与静态库

---
+++ docs/conf.py
@@ -0,0 +1,74 @@
+﻿# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = "MFC编程最佳实践(2026版)"
+copyright = "2026, 辛亚平"
+author = "辛亚平"
+
+
+# -- General configuration ---------------------------------------------------
+# -- General configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosectionlabel",
+]
+
+intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
+templates_path = ["_templates"]
+
+language = 'zh_CN'
+
+numfig=True
+numfig_format = {
+    'code-block': 'Listing %s',
+    'figure': '图 %s',
+    'section': '章节 %s',
+    'table': '表 %s',
+}
+
+# -- Options for EPUB output
+epub_show_urls = "footnote"
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,16 @@
+﻿.. MFC编程最佳实践(2026版) documentation master file, created by
+   sphinx-quickstart on Wed Jan 21 20:35:27 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+.. 这一页是目录树
+
+技术笔记(2026版) 
+=====================================
+
+.. toctree::
+   :maxdepth: 1
+   :caption: QT
+   :numbered:
+   
+   QT/260121-VS2026-BuildQT5.rst
